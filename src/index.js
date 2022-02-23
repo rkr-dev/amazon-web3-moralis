29617a7823cce89c7347247072d6c32610c248c1
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-02-23 07:50:10 -0800

    Installed Dependencies
     - Setup Moralis  Provide
    
     - environment Variabls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MoralisProvider } from 'react-moralis';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
 import './index.css'
 
-ReactDOM.render(<React.StrictMode>
-  <App/>
-</React.StrictMode>,document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider
+      appId={process.env.REACT_APP_APPLICATION_ID}
+      serverUrl={process.env.REACT_APP_SERVER_URL}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
