f9d09f75070d90cf9d91e425bbb2a90e09b90d6a
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-02-24 07:53:28 -0800

    Header Component
     - Header Component
    
     - Project Images Addded

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import styles from './App.module.css';
 import { NotFound } from './components/NotFound/NotFound';
 import Category from './pages/Category';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import './App.css';
 
 export default function App() {
   return (
-    <div className={styles.App}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Category />} />
-        <Route path="/products/:id" element={<Product />} />
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/categories' element={<Category />} />
+      <Route path='/products/:id' element={<Product />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { PageHeader, Button, Input, Space, Badge } from 'antd';
+import { ShoppingCartOutlined, MenuOutlined } from '@ant-design/icons';
+import { useMoralis } from 'react-moralis';
+import {
+  bookstore as BookStore,
+  logo as Amazon,
+  usa as USA,
+} from '../../images';
+import './Header.css';
+
+export const Header = () => {
+  const { authenticate, account } = useMoralis();
+  const { Search } = Input;
+  return (
+    <div className='site-page-header-ghost-wrapper'>
+      <PageHeader
+        ghost={false}
+        extra={[
+          <img src={Amazon} className='logo' key='1' />,
+          <img src={BookStore} className='logo' key='2' />,
+          <Search
+            placeholder='Find A Product'
+            enterButton
+            className='searchBar'
+            key='3'
+          />,
+          <Button
+            className='login'
+            type='primary'
+            onClick={() => authenticate()}
+            key='4'
+          >
+            {account ? (
+              <span>{account.slice(0, 5)}...</span>
+            ) : (
+              <span>login</span>
+            )}
+          </Button>,
+          <Space size={'large'} key='5'>
+            <Badge count={0} showZero>
+              <span className='header-buttons'>
+                <ShoppingCartOutlined className='header-icon' />
+                Cart
+              </span>
+            </Badge>
+            <Space className='header-buttons' size={'small'}>
+              <img src={USA} alt='region' className='flag'></img>▾
+            </Space>
+          </Space>,
+        ]}
+      ></PageHeader>
+    </div>
+  );
+};
