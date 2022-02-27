2e0b3f532f7f06e598bdf5b4a2502c28152cddd8
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-02-26 19:13:50 -0800

    Minor Fixes
     - navigation

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
 import { Card, Rate } from 'antd';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './Product.css';
 
-export const Product = ({image,name,price,id,rating}) => {
+export const Product = ({ image, name, price, id, rating }) => {
+  const location = useLocation();
+  console.log('location: ', location);
   return (
     <Card>
       <div style={{ display: 'flex' }}>
@@ -11,12 +14,22 @@ export const Product = ({image,name,price,id,rating}) => {
           <p className='title'>{name}</p>
           <Rate value={rating} disabled={true}></Rate>
           <h2> ${price}</h2>
-          <p>Ships to Your Location</p>
-          <Link to={`/products/${id}`} className='login'>
-            Got to Product Page
-          </Link>
+          {
+            (location.pathname = '/categories' ? (
+              <p>Ships to Your Location</p>
+            ) : (
+              <p>No Import Fees & Free Shipping Included</p>
+            ))
+          }
+          {
+            (location.pathname = '/categories' && (
+              <Link to={`/products/${id}`} className='login'>
+                Got to Product Page
+              </Link>
+            ))
+          }
         </div>
       </div>
     </Card>
   );
-}
+};
